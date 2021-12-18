6e9a04673dd69e77a4a0b21753f0bcbd26e8278d
snsk <snsk@users.noreply.github.com>
2021-12-18 14:55:14 +0900

    Update main.py

--- check_deck_reservement/main.py
+++ check_deck_reservement/main.py
@@ -7,14 +7,23 @@ get_driver.install()
 def driver_init():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
+    options.add_argument('--log-level=3')
     return webdriver.Chrome(options=options)
 
 driver = driver_init()
-driver.get('https://store.steampowered.com/steamdeck')
+driver.implicitly_wait(10)
 
-if len(driver.find_elements_by_class_name('reservations_noreserve_UsTrT'))>0:
-    print(driver.find_element_by_class_name('reservations_noreserve_UsTrT').text)
+driver.get('https://www.steamdeck.com/ja/')
+
+expect_text = 'Steam Deckは、2022年2月より、アメリカ、カナダ、欧州連合、イギリスで出荷が開始されます。その後、他の地域でも出荷予定です。今後のお知らせをお楽しみに。'
+if len(driver.find_elements_by_id('availability'))>0:
+    actual_text = driver.find_element_by_xpath('/html/body/div[3]/section[12]/div/div[2]/p').text
 else:
-    print("Steam deck reservement has already started!")
+    print("Steam Deck availability region has changed!")
+
+if expect_text == actual_text:
+    print('Steam Deck does not available my region ...')
+else:
+    print("Steam Deck availability region has changed!")
 
 driver.quit()
